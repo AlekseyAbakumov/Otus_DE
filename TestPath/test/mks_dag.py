5944863ae5a18d4f3d169afa623f4d292619652c
Aleksey Starostin <a-starostin@mail.ru>
2022-07-05 21:58:26 +0300

    В целом прод - код готов

--- TestPath/test/mks_dag.py
+++ TestPath/test/mks_dag.py
@@ -1,35 +1,40 @@
+from airflow import DAG
+import pendulum
+from airflow.operators.dummy import DummyOperator
+from airflow.operators.python_operator import PythonOperator
 import requests
 import psycopg2
 
-# from config_prod import host, port, user, password, db_name
-
-host = "rc1b-fsebsbna7j41vi8o.mdb.yandexcloud.net"
-port = 6432
-user = "srv-user"
-password = "12345678"
-db_name = "analytics"
-
-def getjson(url):
+def getjson():
+    url="http://api.open-notify.org/iss-now.json"
     request=requests.get(url)
     api_json=request.json()
     return api_json
 
-def pars_dict(mks_dict):
-    yy=mks_dict
-    print(yy)
-    print(type(yy))
-    iss_position=mks_dict['iss_position']
+def pars_dict(**kwargs):
+    ti = kwargs['ti']
+    api_json=ti.xcom_pull(task_ids='getjson')
+    iss_position=api_json['iss_position']
     latitude=iss_position['latitude']
     longitude=iss_position['longitude']
 
-    timestamp=mks_dict['timestamp']
+    timestamp=api_json['timestamp']
 
     return latitude, longitude, timestamp
 
-def post_data(x):
+def post_data(**kwargs):
+    ti = kwargs['ti']
+    x=ti.xcom_pull(task_ids='pars_dict')
+
     timestamp=x[2]
     latitude=x[0]
     longitude=x[1]
+
+    host = "rc1b-fsebsbna7j41vi8o.mdb.yandexcloud.net"
+    port = 6432
+    user = "srv-user"
+    password = "12345678"
+    db_name = "analytics"
 
     try:
         # connect to exist database
@@ -55,6 +60,14 @@ def post_data(x):
             connection.close()
             print("[INFO] PostgreSQL connection closed")
 
-mks_coordinat=getjson("http://api.open-notify.org/iss-now.json")
-x=pars_dict(mks_coordinat)
-post_data(x)
+with DAG(
+    dag_id='Where_MKS',
+    schedule_interval=None,
+    start_date=pendulum.datetime(2022, 1, 1)
+    ) as dag:
+
+    task_getjson = PythonOperator(task_id="getjson", python_callable=getjson)
+    task_pars_dict = PythonOperator(task_id="pars_dict", python_callable=pars_dict)
+    task_post_data = PythonOperator(task_id="post_data", python_callable=post_data)
+    
+    task_getjson >> task_pars_dict >> task_post_data
